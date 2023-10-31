7c98a14e0595359361127bc91242ceab02b3d13e
Dmytro  Goncharov <124349293+DG4848@users.noreply.github.com>
2023-10-31 14:01:39 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,21 +1,31 @@
-// Importing or defining constants and utility functions for salt retrieval
-const SALT_KEY = '123PassSalt';
+import { retrieveFromStorage, SALT_KEY, USER_PATH_KEY } from './storage.js';
+import { hashMasterPassphrase } from './crypto.js';
+let originalTabId; // The ID of the tab that opened the popup
 
 function retrieveSalt(callback) {
-    chrome.storage.local.get([SALT_KEY], function(result) {
-        if (result[SALT_KEY]) {
-            callback(result[SALT_KEY]);
+    // Ask the background script for the current salt.
+    chrome.runtime.sendMessage({ action: "GET_SALT" }, function(response) {
+        if (response && response.salt) {
+            console.log("Using in-memory salt:", response.salt);
+            callback(response.salt);
         } else {
-            console.log("No salt found in storage.");
-            callback(null);
+            console.log("No in-memory salt found. Retrieving from storage.");
+            chrome.storage.local.get([SALT_KEY], function(result) {
+                if (result[SALT_KEY]) {
+                    callback(result[SALT_KEY]);
+                } else {
+                    console.log("No salt found in storage.");
+                    callback(null);
+                }
+            });
         }
     });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Password visibility toggle
     const passwordInput = document.getElementById('EasyPass');
     const togglePasswordButton = document.getElementById('togglePasswordButton');
+    const generatePassword = document.getElementById('generate-password');
 
     togglePasswordButton.addEventListener('click', function() {
         if (passwordInput.type === 'password') {
@@ -28,30 +38,38 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        let tabUrl;
-        // Check if a tab URL was passed as a parameter
-        const urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.has('tabUrl')) {
-            tabUrl = urlParams.get('tabUrl');
-        } else {
-            // If no tab URL parameter, fallback to using the active tab's URL
-            tabUrl = tabs[0].url;
-        }
-        
-        let url = new URL(tabUrl);
-        let domain = url.hostname;
+      const urlParams = new URLSearchParams(window.location.search);
+  
+      // Always get the tabId from the parameter, regardless of whether 'tabUrl' exists.
+      if (urlParams.has('tabId')) {
+          originalTabId = parseInt(urlParams.get('tabId'));
+      } else {
+          originalTabId = tabs[0].id;
+      }
+  
+      let tabUrl = urlParams.has('tabUrl') ? urlParams.get('tabUrl') : tabs[0].url;
+  
+      let url = new URL(tabUrl);
+      let domain = url.hostname;
+  
+      document.getElementById('domain').value = domain;
+  });
+  
 
-        // Display the domain in the 'domain' input field
-        document.getElementById('domain').value = domain;
+    // Event listener for pressing Enter key
+    passwordInput.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Prevent default form submission
+            generatePassword.click(); // Simulate a button click
+        }
     });
 
     // Listener for password generation
-    document.getElementById('generate-password').addEventListener('click', async function() {
+    generatePassword.addEventListener('click', async function() {
         const easyPass = passwordInput.value;
-        const useDomain = document.getElementById('use-domain').checked; // Corrected the checkbox's ID
-        const desiredLength = Number(document.getElementById('password-length').value); // Corrected the input's ID
+        const useDomain = document.getElementById('use-domain').checked;
+        const desiredLength = Number(document.getElementById('password-length').value);
 
-        // Retrieve salt from storage
         retrieveSalt(async function(salt) {
             if(!salt) {
                 alert('Salt not found. Please configure setup options.');
@@ -59,8 +77,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
             let combinedString = easyPass;
-            
-            // If the user wants to use the domain in the password generation
+
             if (useDomain) {
                 const domain = document.getElementById('domain').value;
                 combinedString += domain;
@@ -68,15 +85,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
             combinedString += salt;
 
-            // Hash the combined string
-            // Assuming hashMasterPassphrase is imported or defined elsewhere in popup.js
             const hashed = await hashMasterPassphrase(combinedString);
 
-            // Create strong password
-            let strongPassword = hashed.substring(0, desiredLength - 1) + '&';
+            let strongPassword = hashed.substring(0, desiredLength);
 
-            // Display the password for the user to copy
-            document.getElementById('GeneratedPasswordOutput').value = strongPassword;
+            document.getElementById('fillCredentials').textContent = strongPassword;
+
         });
     });
+
+// Event listener for filling credentials
+document.getElementById('fillCredentials').addEventListener('click', function() {
+  console.log('Button clicked!');
+  const generatedPassword = this.textContent;
+
+
+  // Copy to clipboard
+  navigator.clipboard.writeText(generatedPassword).then(function() {
+      console.log('Password copied to clipboard.');
+  }, function(err) {
+      console.error('Could not copy password: ', err);
+  });
+
+    chrome.storage.local.get('savedUsername', function(data) {
+    const username = data.savedUsername || 'DefaultUsername'; // Use a default username if not found.
+
+    console.log('Sending message to content script:', { action: 'fillCredentials', username: username, password: generatedPassword });
+
+
+    // Send a message to the content script to fill in the fields
+    console.log('Sending message to tab ID:', originalTabId);
+    chrome.tabs.sendMessage(originalTabId, { action: 'fillCredentials', username: username, password: generatedPassword });
+    });
+  
+  });
 });
