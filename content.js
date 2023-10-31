78cd5ba320fb91ed44ada2a2d9ba1c314a10d6a0
Dmytro  Goncharov <124349293+DG4848@users.noreply.github.com>
2023-10-31 13:57:47 +0800

    Update content.js

--- content.js
+++ content.js
@@ -1,26 +1,26 @@
-// This function inserts a tooltip next to a password field.
-function insertTooltipForPasswordField(passwordField) {
+    // This function inserts a tooltip next to a password field.
+    function insertTooltipForPasswordField(passwordField) {
     console.log('Inserting tooltip for password field.');
 
     // Create the tooltip div.
     const tooltipDiv = document.createElement('div');
     tooltipDiv.style.position = 'absolute';
-    tooltipDiv.style.background = '#333';
+    tooltipDiv.style.background = '#611396';
     tooltipDiv.style.color = 'white';
-    tooltipDiv.style.padding = '5px';
-    tooltipDiv.style.borderRadius = '5px';
+    tooltipDiv.style.padding = '2px';
+    tooltipDiv.style.borderRadius = '4px';
     tooltipDiv.style.zIndex = '1000000'; // Set a very high z-index value to ensure visibility.
-    tooltipDiv.innerText = 'Use 123Pass';
+    tooltipDiv.innerText = '';
 
     // Create the "OK" button inside the tooltip.
     const okButton = document.createElement('button');
-    okButton.innerText = 'OK';
-    okButton.style.marginLeft = '10px';
+    okButton.innerText = '123';
+    okButton.style.marginLeft = '1px';
     okButton.onclick = function() {
         console.log('"OK" button clicked. Sending message to open 123PassPopup.');
         chrome.runtime.sendMessage({
             action: 'open123PassPopup',
-            tabUrl: window.location.href  // Get the current page's URL
+            tabUrl: window.location.href,  // Get the current page's URL
         });
     };
 
@@ -32,21 +32,36 @@ function insertTooltipForPasswordField(passwordField) {
 
     // Now, position the tooltip centered vertically relative to the password field.
     const rect = passwordField.getBoundingClientRect();
-    tooltipDiv.style.top = (rect.top + window.scrollY + (rect.height / 2) - (tooltipDiv.offsetHeight / 2)) + 'px';
-    tooltipDiv.style.left = (rect.right + 10) + 'px'; // 10px distance from the password field.
+    tooltipDiv.style.top = (rect.top + window.scrollY + (rect.height / 2 -1) - (tooltipDiv.offsetHeight / 2)) + 'px';
+    tooltipDiv.style.left = (rect.right - 100) + 'px'; // px distance from the password field.
 }
 
-// Check for password fields in the DOM and insert tooltips.
-const passwordFields = document.querySelectorAll('input[type="password"]');
-console.log(`Found ${passwordFields.length} password fields.`);
-passwordFields.forEach(insertTooltipForPasswordField);
+    // Check for password fields in the DOM and insert tooltips.
+    const passwordFields = document.querySelectorAll('input[type="password"]');
+    console.log(`Found ${passwordFields.length} password fields.`);
+    passwordFields.forEach(insertTooltipForPasswordField);
 
-// Listen for the message from the background script.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log('Received message:', request);
+
     if (request.action === 'showTooltipForPasswordField') {
         console.log('Received message to show tooltip for password field.');
         // Find the password fields on the current page.
         const passwordFields = document.querySelectorAll('input[type="password"]');
         passwordFields.forEach(insertTooltipForPasswordField);
+
+    } else if (request.action === 'fillCredentials') {
+        const usernameFields = document.querySelectorAll('input[type="email"], input[type="text"]');
+        const passwordFields = document.querySelectorAll('input[type="password"]');
+
+        if (usernameFields.length) {
+            usernameFields[0].value = request.username;
+        }
+
+        if (passwordFields.length) {
+            passwordFields[0].value = request.password;
+        }
     }
 });
+
+
